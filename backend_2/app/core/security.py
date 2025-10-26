d0b01f9640d1966a74625e26898d68c013d66960
Pablo Barros <pablovin@gmail.com>
2025-10-26 19:54:16 +0100

    updated bcrypt

--- backend_2/app/core/security.py
+++ backend_2/app/core/security.py
@@ -37,7 +37,11 @@ from app.core.config import get_settings
 
 settings = get_settings()
 
-pwd_context = CryptContext(schemes=["bcrypt_sha256", "bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt_sha256", "bcrypt"],
+    deprecated="auto",
+    bcrypt__truncate_error=False,
+)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
